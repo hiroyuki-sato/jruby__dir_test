d0ec5f1fd401e61a9c8d24d367c3f6b1b74261f3
Hiroyuki Sato <hiroysato@gmail.com>
2017-05-17 21:15:25 +0900

    Liquid-4.0.0

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,30 +1,10 @@
-#require 'colorize'
-#java_import 'org.slf4j.Logger'
-#java_import 'org.slf4j.LoggerFactory'
-
 puts JRUBY_VERSION
 puts RUBY_VERSION
-
-#logger = LoggerFactory.getLogger('demo')
-
-=begin
-puts "__dir__: #{__dir__}"
-puts "#{__FILE__}"
-puts "File.dirname: #{File.dirname(__FILE__)}"
-puts "-" * 20
-logger.info "Ruby version: #{RUBY_VERSION}"
-logger.info "Ruby platform: #{RUBY_PLATFORM}"
-logger.info "Current file: #{__FILE__}"
-puts "-" * 20
-
-puts "Roses are red".red
-puts "Violets are blue".blue
-puts "I can use JRuby/Gradle".green
-puts "And now you can too!".yellow
-=end
-
 
 require 'liquid'
 require 'pp'
 pp Liquid::Continue
-pp Liquid::Continue
+
+#p Dir["/private/tmp/jruby-gradle-plugin/examples/self-executing-jar!uri:classloader:/gems/liquid-4.0.0/lib"].to_a
+#p Dir["/private/tmp/jruby-gradle-plugin/examples/self-executing-jar/uri:classloader:/gems/liquid-4.0.0/lib"].to_a
+#p Dir["uri:classloader:/gems/liquid-4.0.0/lib"].to_a

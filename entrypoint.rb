cfe4bdf8069c25435f24eec43bd2da0e88c6864f
Hiroyuki Sato <hiroysato@gmail.com>
2017-05-18 01:20:55 +0900

    jruby_q

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,6 +1,11 @@
 puts JRUBY_VERSION
 puts RUBY_VERSION
 
+p Dir.exist?("uri:classloader:/gems/liquid-4.0.0/lib")
+p Dir.exist?("/private/tmp/jruby__dir_test/build/libs/jruby__dir_test-jruby.jar!uri:classloader:/gems/liquid-4.0.0/lib")
+p Dir.exist?("/private/tmp/jruby__dir_test/build/libs/jruby__dir_test-jruby.jar!/gems/liquid-4.0.0/lib")
+p Dir.exist?("/private/tmp/jruby__dir_test/uri:classloader:/gems/liquid-4.0.0/lib")
+
 require 'liquid'
 require 'pp'
 pp Liquid::Continue
